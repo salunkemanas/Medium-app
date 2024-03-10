b5427f5ca96155587633b44efb72e711d807b6c1
Cactusjuice <96051596+salunkemanas@users.noreply.github.com>
2024-03-10 21:43:15 +0530

    Update Auth.tsx

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -79,7 +79,7 @@ interface LabelledInputType{
 function LabelledInput({label, placeholder, onChange}: LabelledInputType){
     return <div>
             <label className=" block mb-2 text-sm font-medium text-black pt-4">{label}</label>
-            <input type="text" onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
+            <input type="text" onChange={onChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder={placeholder} required />
         </div>
     
 }
