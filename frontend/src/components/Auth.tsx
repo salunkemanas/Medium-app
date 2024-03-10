5b1add3c89472f3a01bb2e63af8c3abee7207e06
Cactusjuice <96051596+salunkemanas@users.noreply.github.com>
2024-03-10 21:44:54 +0530

    Update Auth.tsx

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -59,7 +59,7 @@ export const Auth = ({type}: {type: "Signup" | "Signin" }) => {
                         password: e.target.value
                     }))
                 }} />
-                <button onClick={sendRequest} type="button" className="mt-8 text-white w-full bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">{type === "Signup"? "Sign up" : "Sign in"}</button>
+                <button onClick={sendRequest} type="button" className="mt-8 text-white w-full bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">{type === "Signup"? "Sign up" : "Sign in"}</button>
 
             </div>
             </div>
