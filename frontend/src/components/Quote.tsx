c596cc34c510c73e9ae296f091b3e7d681c6b3e7
Cactusjuice <96051596+salunkemanas@users.noreply.github.com>
2024-03-12 12:39:26 +0530

    Update Quote.tsx

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -2,7 +2,7 @@ export const Quote = () => {
     return <div className="bg-slate-200	h-screen flex justify-center flex-col">
         <div className="flex justify-center">
             <div className="max-w-md">
-                <div className="text-3xl font-bold">hi from quote component</div>
+                <div className="text-3xl font-bold">The customer service I experienced was exceptional, the support team surpassed all expectations in addressing my concerns, demonstrating a remarkable commitment to excellence and customer satisfaction.</div>
                 <div className="max-w-md text-xl font-semibold text-left top-4">Manoj Malhotra</div>
                 <div className="max-w-md text-sm font-light text-left text-slate-400">CEO | Amce corp</div>
                 
